67360ace9fb15d440a5d67bd3af3425d609fa211
volkovanton71 <volkovanton71@yandex.ru>
2025-01-09 14:06:01 +0300

    save

--- first_app/main.cpp
+++ first_app/main.cpp
@@ -1,25 +1,21 @@
 #include <iostream>
+#include "proba.cpp";
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+using namespace std;
+
+
 int main() {
+    int number;
     // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
     // <b>lang</b> variable name to see how CLion can help you rename it.
-    const auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    cout << "Input number:" ;
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
-    }
+    cin >> number;
+    // cin.ignore();
+    cout << number << endl;
+    cin.get();
 
+    func_out("Hello from main");
     return 0;
 }
 
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
---
+++ first_app/proba.cpp
@@ -0,0 +1,8 @@
+#include "proba.h";
+
+#include <iostream>
+#include <ostream>
+
+void func_out(const string& s) {
+    cout << s << endl;
+}
