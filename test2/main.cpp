201ba402c7630d8c57f8523c49e43f450a3d62f4
volkovanton71 <volkovanton71@yandex.ru>
2025-01-10 17:13:31 +0300

    save

--- test2/main.cpp
+++ test2/main.cpp
@@ -5,17 +5,22 @@
     09.01.2024
 */
 
+#include <cmath>
 #include <iostream>
 using namespace std;
-
+#include <bitset>
 
 int main(int argc, char *argv[]) {
+    int a = 10, b = 4;
 
-    unsigned short int arr[] = {};
-    arr[2] = 22222222222222;
-    cout << arr[2] << endl;
+    int c = a / b; // 2
+    float d = static_cast<float>(a) / static_cast<float>(b); // 2.5
+    float e = a / static_cast<float>(b); // 2.5
+    float f = static_cast<float>(a) / b; // 2.5
+
+    cout << c << endl;
+    cout << d << endl;
+    cout << e << endl;
+    cout << f << endl;
     return 0;
 }
-
-
-### Операции присваивания в С++
