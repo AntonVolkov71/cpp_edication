0cb63eacf68caf77c3a6fc5d57c46d98256373a4
Антон Волков <volkovanton71@yandex.ru>
2025-01-29 17:39:12 +0300

    save

--- tree/cmake-build-debug/cmake_install.cmake
+++ tree/cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/volko/Documents/2. Edication/1. cpp/test2
+# Install script for directory: C:/Users/volko/Documents/2. Edication/1. cpp/tree
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/test2")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/tree")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "C:/Users/volko/Documents/2. Edication/1. cpp/test2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/volko/Documents/2. Edication/1. cpp/tree/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
