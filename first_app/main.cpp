76884ba78534ced6640fc18ead8540ef9a8fa6cf
Антон Волков <volkovanton71@yandex.ru>
2025-01-31 16:44:49 +0300

    save

--- first_app/main.cpp
+++ first_app/main.cpp
@@ -1,29 +1,80 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <string>
+#include <map>
 
 using namespace std;
 
-string PurifyWord(const string& word) {
-    string pure_word;
-    for (const char c : word) {
-        if (c != '-' && c != '+' && c != '!') {
-            pure_word.push_back(c);
+// Определяет, будет ли слово палиндромом
+// text может быть строкой, содержащей строчные символы английского алфавита и пробелы
+// Пустые строки и строки, состоящие только из пробелов, — это не палиндромы
+bool IsPalindrome(const string &text) {
+    bool res = false;
+
+    if (text.empty()) {
+        return false;
+    }
+
+    int left = 0;
+    int right = static_cast<int>(text.size() - 1);
+
+//    if ((left == right) && (text[left] == text[right])) {
+//        return true;
+//    }
+
+    while (left != right) {
+        if (!text[left]) {
+            left++;
+            continue;
+        }
+
+        if (!text[right]) {
+            right--;
+            continue;
+        }
+
+        if (text[left] == text[right]) {
+            left++;
+            right--;
+            res= true;
+        } else {
+            return false;
         }
     }
-    return pure_word;
+
+    // Напишите недостающий код
+    return res;
 }
 
-
 int main() {
-    const vector<string> words = {"!Cat"s, "-pushisty"s};
-    vector<string> pure_words;
+//    string text;
+//    getline(cin, text);
 
-    for (const string& word : words) {
-        pure_words.push_back(PurifyWord(word));
+    map<string, bool> tests;
+
+    tests.insert({"", false});
+    tests.insert({"     ", false});
+    tests.insert({"anton", false});
+    tests.insert({"tonot", true});
+    tests.insert({"tt tt", true});
+    tests.insert({"tt t t   ", true});
+    tests.insert({"a", true});
+    tests.insert({"aa", true});
+    tests.insert({"  a   ", true});
+
+
+    for (const auto &test: tests) {
+        cout << "Test: ";
+
+        string isRes = IsPalindrome(test.first) == test.second
+                       ? "OK"
+                       : test.first + " Error";
+
+        cout << isRes << endl;
     }
 
-    for (const string& word : pure_words) {
-        cout << word << endl;
-    }
+//    if (IsPalindrome(text)) {
+//        cout << "palindrome"s << endl;
+//    } else {
+//        cout << "not a palindrome"s << endl;
+//    }
 }
