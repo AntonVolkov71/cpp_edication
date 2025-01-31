f665eefaa2bfcbfaf466ea8e1ca6bae604df25be
volkovanton71 <volkovanton71@yandex.ru>
2025-01-31 12:13:56 +0300

    save

--- first_app/main.cpp
+++ first_app/main.cpp
@@ -1,67 +1,29 @@
-ad#include <iostream>
+#include <iostream>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-enum class AnimalSortKey {
-    AGE,     // по полю age
-    WEIGHT,  // по полю weight
-    RELATIVE_WEIGHT  // по weight / age
-};
-
-struct Animal {
-    string name;
-    int age;
-    double weight;
-};
-
-void PrintNames(const vector<Animal>& animals) {
-    for (const Animal& animal : animals) {
-        cout << animal.name << ' ';
+string PurifyWord(const string& word) {
+    string pure_word;
+    for (const char c : word) {
+        if (c != '-' && c != '+' && c != '!') {
+            pure_word.push_back(c);
+        }
     }
-    cout << endl;
-}
-auto key_mapper_func = [](const Animal& animal) { return animal.name; };
-
-// как написать SortBy?
-template <typename Container, typename KeyMapper>
-void SortBy(Container& container, KeyMapper key_mapper, bool is_reverse = false) {
-    sort(container.begin(), container.end(),[key_mapper, is_reverse](const auto &lhs, const auto &rhs){
-        return is_reverse
-        ? key_mapper(lhs) < key_mapper(rhs)
-        : key_mapper(lhs) > key_mapper(rhs);
-    });
+    return pure_word;
 }
 
-void SortBy(vector<Animal>& animals, AnimalSortKey sort_key, bool reverse = false) {
-    switch (sort_key) {
-        case AnimalSortKey::AGE:
-            // возвращается void, но return помогает сразу выйти из функции
-            return SortBy(animals, [](const auto& x) { return x.age; }, reverse);
-        case AnimalSortKey::WEIGHT:
-            return SortBy(animals, [](const auto& x) { return x.weight; }, reverse);
-        case AnimalSortKey::RELATIVE_WEIGHT:
-            return SortBy(animals, [](const auto& x) { return x.weight / x.age; }, reverse);
-    }
-}
 
 int main() {
-    vector<Animal> animals = {
-            {"Mura"s,   10, 5},
-            {"Bela"s,   5,  1.5},
-            {"Geor"s, 2,  4.5},
-            {"Ryri"s,   12, 3.1},
-    };
+    const vector<string> words = {"!Cat"s, "-pushisty"s};
+    vector<string> pure_words;
 
-    PrintNames(animals);
+    for (const string& word : words) {
+        pure_words.push_back(PurifyWord(word));
+    }
 
-    SortBy(animals, key_mapper_func);
-    PrintNames(animals);
-    SortBy(animals, [](const Animal& animal) { return animal.name; }, true);
-    PrintNames(animals);
-    SortBy(animals, [](const Animal& animal) { return -animal.weight; });
-    PrintNames(animals);
-    SortBy(animals, AnimalSortKey::RELATIVE_WEIGHT);
-    PrintNames(animals);
+    for (const string& word : pure_words) {
+        cout << word << endl;
+    }
 }
