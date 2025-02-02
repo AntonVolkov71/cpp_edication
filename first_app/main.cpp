1737bc7b402b33cf7c2971565a67f5e3d1922f47
volkovanton71 <volkovanton71@yandex.ru>
2025-02-02 22:04:42 +0300

    S

--- first_app/main.cpp
+++ first_app/main.cpp
@@ -1,31 +1,50 @@
-#include <numeric>
 #include <iostream>
-#include <vector>
-#include <valarray>
+#include <stdexcept>
+#include <string>
 
 using namespace std;
 
-class Greeter {
-public:
-    Greeter(const string& name)
-            : name_(name)
-    {
-        cout << "Hello, "s << name_ << "!"s << endl;
-    }
-
-    ~Greeter() {
-        // Поля класса внутри деструктора еще не уничтожены, и к ним можно обращаться
-        cout << "Goodbye, "s << name_ << "!"s << endl;
-    }
-private:
-    string name_;
-};
-
-Greeter ReturnGreeter() {
-    return {"result"s};
+// Удаляет файл. В случае ошибки может выбросить исключение
+void DeleteFile(const string &path) {
+    throw invalid_argument("no open "s + path);
 }
 
+// Управляет временным файлом на диске. При своём разрушении удаляет временный файл
+class TemporaryFile {
+public:
+    explicit TemporaryFile(const string &path)
+            : path_(path) {
+    }
+
+    const string &GetPath() const {
+        return path_;
+    }
+
+    ~TemporaryFile() {
+        int retries = 10;
+        while (retries > 0) {
+            try {
+                DeleteFile(path_);
+                break;
+            } catch (exception &e) {
+                cout << e.what() << endl;
+                // Возможно, повезёт на следующей попытке,
+                // если ошибка удаления файла была временной.
+                // Например, в момент удаления файл проверялся антивирусом
+                --retries;
+            }
+        }
+    }
+
+private:
+    string path_;
+};
+
 int main() {
-    ReturnGreeter();
-    Greeter body("body"s);
+    TemporaryFile tmpFile("temp.txt"s);
+
+    return 0;
+    /* Что-нибудь делаем с этим файлом */
+
+    // При выходе из функции деструктор TemporaryFile постарается удалить файл с диска
 }
