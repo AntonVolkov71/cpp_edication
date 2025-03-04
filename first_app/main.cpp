786114dd09198d41e5112f4dbaa445596d936235
Антон Волков <volkovanton71@yandex.ru>
2025-03-04 09:47:04 +0300

    s

--- first_app/main.cpp
+++ first_app/main.cpp
@@ -1,21 +1,22 @@
-#include <algorithm>
 #include <iostream>
-#include <random>
-
-using namespace std;
-
-void PrintSpacesPositions(string &str) {
-    size_t pos = 0;
-    size_t i;
-
-    while ((i = str.find(" "s, pos)) != string::npos) {
-        cout << i << endl;
-        pos = i + 1;
-    }
-}
+#include <chrono>
+#include <thread>
+#include <iomanip>
 
 int main() {
-    string str = "He said: one and one and one is three"s;
-    PrintSpacesPositions(str);
+    while (true) {
+//         Получаем текущее время
+        auto now = std::chrono::system_clock::now();
+        std::time_t now_c = std::chrono::system_clock::to_time_t(now);
+
+        //         Выводим время в формате ЧЧ:ММ:СС
+        std::cout << std::put_time(std::localtime(&now_c), "%H:%M:%S") << std::endl;
+
+        std::cout << "Hello" << std::flush;
+
+        // Задержка 5 секунд
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+    }
+
     return 0;
 }
---
+++ rational/cmake-build-debug/CMakeFiles/rational.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rational.dir/main.cpp.obj"
+  "CMakeFiles/rational.dir/main.cpp.obj.d"
+  "librational.dll.a"
+  "rational.exe"
+  "rational.exe.manifest"
+  "rational.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rational.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
