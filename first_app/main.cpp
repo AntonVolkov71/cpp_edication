b4bd0d4d0e779bb1ebfcf7a38696a76d648fcadf
Антон Волков <volkovanton71@yandex.ru>
2025-03-14 17:54:27 +0300

    s

--- first_app/main.cpp
+++ first_app/main.cpp
@@ -1,36 +1,44 @@
-#include <iostream>
+#include <cassert>
+#include <stack>
 
 using namespace std;
 
-template<typename F>
-int FindFloor(int n, F drop) {
-    int a = 1, b = n;
-
-    while (a != b) {
-        int m = (a + b) / 2;
-
-        if (drop(m)) {
-            b = m;
-        } else {
-            a = m + 1;
-        }
+// моделируем очередь с помощью двух стеков
+template <class T>
+class QueueFromStack {
+public:
+    void Push(const T& e) {
+        stack1_.push(e);
     }
 
-    return a;
-}
+    T Pop() {
+        if (stack2_.empty()) {
+            while (!stack1_.empty()) {
+                stack2_.push(stack1_.top());
+                stack1_.pop();
+            }
+        }
+
+        assert(!stack2_.empty());
+
+        T top = stack2_.top();
+        stack2_.pop();
+        return top;
+    }
+
+private:
+    stack<T> stack1_;
+    stack<T> stack2_;
+};
 
 int main() {
-    int n, t;
-    cout << "Enter n and target floor number: "s << endl;
-    cin >> n >> t;
-
-    int count = 0;
-    int found = FindFloor(n, [t, &count](int f) {
-        ++count;
-        return f >= t;
-    });
-
-    cout << "Found floor "s << found << " after "s << count << " drops"s;
-
-    return 0;
+    QueueFromStack<int> q;
+    q.Push(1);
+    q.Push(2);
+    q.Push(3);
+    q.Push(4);
+    assert(q.Pop() == 1);
+    assert(q.Pop() == 2);
+    assert(q.Pop() == 3);
+    assert(q.Pop() == 4);
 }
