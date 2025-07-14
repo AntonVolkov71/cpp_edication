0025262719b78f6be8091118c914c35f84ccd515
Антон Волков <volkovanton71@yandex.ru>
2025-07-14 11:03:21 +0300

    s

--- first_app/main.cpp
+++ first_app/main.cpp
@@ -1,22 +1,41 @@
+#include <algorithm>
+#include <cassert>
+#include <functional>
 #include <iostream>
+#include <string_view>
+
 using namespace std;
 
+template<typename T>
+void TestComparator(T comp, std::string_view name) {
+    vector v1{3, 1, 4, 555, 3, 4, 6, 3, 345, 53, 5, 345, 3};
+    vector v2 = v1;
 
+    sort(v1.begin(), v1.end());
+    sort(v2.begin(), v2.end(), comp);
 
-void gen_binary(int n, string prefix, int left) {
-    if (n == 0) {
-        cout << string(left++, ' ') << prefix << endl;
-
-        return;
-    } else {
-        cout <<string(left++, ' ') <<  prefix << endl;
-
-//        gen_binary(n - 1, prefix + "0",left);
-        gen_binary(n - 1, prefix + "1", left);
-    }
+    cout << "Comparator "sv << name << ": "sv << (v1 == v2 ? "OK"sv : "Fail"sv) << endl;
 }
 
+bool CmpFunction(int x, int y) {
+    return x < y;
+}
+
+struct CmpObject {
+    bool operator()(int x, int y) const {
+        return x < y;
+    }
+};
+
 int main() {
-    gen_binary(3, "", 0);
-    return 0;
+    auto cmp_lambda = [](int x, int y) {
+        return x < y;
+    };
+
+    TestComparator(CmpFunction, "Function "sv);
+    TestComparator(CmpObject(), "Object "sv);
+    TestComparator(std::less<>(), "std::less"sv);
+    TestComparator(cmp_lambda, "Lambda Function"sv);
+    TestComparator(std::ref(cmp_lambda), "Lambda Function by ref"sv);
+    TestComparator(std::function(CmpFunction), "std::function"sv);
 }
